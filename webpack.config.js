2bd2313e8a8648a12055c7c685a05e9ca59b6690
Karim Slimani <karim@playmedia.fr>
2017-05-30 18:46:57 +0200

    chore(build): use webpack2 with dev server and add yarn lock file

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,74 @@
-var path = require('path')
-var webpack = require('webpack')
-var Clean = require('clean-webpack-plugin')
+// Webpack 2 configuration
+const path = require('path')
+const webpack = require('webpack')
+const NotifierPlugin = require('webpack-notifier')
 
-var plugins = []
-var babelCompact = false
+var outputFile, plugins = []
 
-if (process.env.npm_lifecycle_event === 'dist-min') {
-  // *** Uncomment the following lines to suppress UglifyJS warnings ***
-  // plugins.push(new webpack.optimize.UglifyJsPlugin({
-  //   compress: {warnings: false},
-  //   output: {comments: false}
-  // }))
-  babelCompact = true
+if (process.env.npm_lifecycle_event === 'dist') {
+  outputFile = 'clappr-google-ima-html5-preroll-plugin.min.js'
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    output: {
+      comments: false,
+    },
+  }))
 } else {
-  plugins.push(new Clean(['dist'], {verbose: false}))
+  outputFile = 'clappr-google-ima-html5-preroll-plugin.js'
 }
+
+plugins.push(new NotifierPlugin({
+  title: outputFile,
+  alwaysNotify: true,
+  // contentImage: path.resolve(__dirname, 'path/to/image.png')
+}))
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: outputFile,
+    library: 'ClapprGoogleImaHtml5PrerollPlugin',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        options: {
+          presets: ['es2015'],
+          plugins: ['add-module-exports'],
+        },
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [
+                path.resolve(__dirname, "node_modules/compass-mixins/lib"),
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-inline-loader',
+          },
+        ],
+      },
+    ],
+  },
   plugins: plugins,
   externals: {
    clappr: {
@@ -27,33 +78,14 @@ module.exports = {
     root: 'Clappr'
    }
   },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-            compact: babelCompact,
-        }
-      },
-      {
-        test: /\.sass$/,
-        loaders: ['css', 'sass?includePaths[]=' + path.resolve(__dirname, "./node_modules/compass-mixins/lib")],
-      },
-      {
-        test: /\.svg/,
-        loader: 'svg-inline'
-      },
+  devServer: {
+    contentBase: [
+      path.resolve(__dirname, "public"),
     ],
-  },
-  resolve: {
-    extensions: ['', '.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'clappr-google-ima-html5-preroll-plugin.js',
-    library: 'ClapprGoogleImaHtml5PrerollPlugin',
-    libraryTarget: 'umd',
-  },
+    // publicPath: '/js/',
+    disableHostCheck: true, // https://github.com/webpack/webpack-dev-server/issues/882
+    compress: true,
+    host: "0.0.0.0",
+    port: 8080
+  }
 }
