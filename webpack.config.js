10c058e9280a2b5e5ff96ba8b773563eb659e598
Karim Slimani <karim@playmedia.fr>
2019-01-11 16:03:55 +0100

    chore: update webpack configuration
    Add Babel configuration file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,32 @@
-// Webpack 2 configuration
+// Webpack 4 configuration
 const path = require('path')
 const webpack = require('webpack')
-const NotifierPlugin = require('webpack-notifier')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const NotifierPlugin = require('webpack-build-notifier')
 
-var outputFile, plugins = []
+var name = 'clappr-google-ima-html5-preroll-plugin'
+var outputFile, plugins = [], optimization = {}
 
 if (process.env.npm_lifecycle_event === 'dist') {
-  outputFile = 'clappr-google-ima-html5-preroll-plugin.min.js'
-  plugins.push(new webpack.optimize.UglifyJsPlugin({
-    output: {
-      comments: false,
-    },
-  }))
+  outputFile = name + '.min.js'
+  optimization.minimizer = [
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      uglifyOptions: {
+        output: {
+          comments: false,
+        },
+      }
+    }),
+  ]
 } else {
-  outputFile = 'clappr-google-ima-html5-preroll-plugin.js'
+  outputFile = name + '.js'
+  optimization.minimize = false
 }
 
 plugins.push(new NotifierPlugin({
-  title: outputFile,
-  alwaysNotify: true,
-  // contentImage: path.resolve(__dirname, 'path/to/image.png')
+  title: optimization.minimizer ? 'minified ' + name : name,
 }))
 
 module.exports = {
@@ -30,18 +37,17 @@ module.exports = {
     library: 'ClapprGoogleImaHtml5PrerollPlugin',
     libraryTarget: 'umd',
   },
+  optimization: optimization,
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        use: {
+          loader: 'babel-loader'
+        },
         include: [
           path.resolve(__dirname, 'src')
         ],
-        options: {
-          presets: ['es2015'],
-          plugins: ['add-module-exports'],
-        },
       },
       {
         test: /\.sass$/,
